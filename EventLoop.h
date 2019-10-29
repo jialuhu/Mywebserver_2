0650a9aaae572aa41e2a70da9ec69f309b94a594
胡佳露 <974520639@qq.com>
2019-10-29 15:53:29 +0800

    Create the EventLoop

---
+++ EventLoop.h
@@ -0,0 +1,70 @@
+//
+// Created by Jialu  Hu on 2019-07-31.
+//
+
+#ifndef TINY_WEB_EVENTLOOP_H
+#define TINY_WEB_EVENTLOOP_H
+
+#include <vector>
+#include <sys/types.h>
+#include <sys/event.h>
+#include <sys/time.h>
+#include <boost/function.hpp>
+#include <boost/scoped_ptr.hpp>
+#include <memory>
+#include <unistd.h>
+#include <assert.h>
+#include <iostream>
+#include <thread>
+#include "MutexLock.h"
+#include "SocketOpt.h"
+class Epoll;
+class Channel;
+class Kqueue;
+typedef boost::function<void ()> Functor;
+class  EventLoop{
+public:
+    EventLoop();
+    ~EventLoop();
+
+    void loop();
+    void quit();
+    void updateChannel(Channel* channel);
+
+    bool isInLoopThread(){
+        return std::this_thread::get_id()==threadId_;
+    }
+    void runInLoop(const Functor& cb);
+    void doPendingFunctors();
+    void handleread();
+    void queueInLoop(const Functor& cb);
+    void wakeup();
+    void assertInLoopThread()
+    {
+        if (!isInLoopThread())
+        {
+            std::cout << "Thread is non in this pthread!\n";
+        }
+    }
+private:
+    boost::scoped_ptr <Kqueue> Kqueue_;
+    std::shared_ptr<Channel> wakeupChannel_;
+    std::vector<Channel*> activeChannel_;
+    //wakeupFd[0]读
+    //wakeupFd[1]写
+    int wakeupFd[2];
+    bool quit_;
+    bool looping_;
+    bool callingPendingFunctors_;
+
+    std::thread::id threadId_;
+    Mutex mutex_;
+    std::vector<Functor> pendingFunctors_;
+
+
+
+};
+
+
+
+#endif //TINY_WEB_EVENTLOOP_H
