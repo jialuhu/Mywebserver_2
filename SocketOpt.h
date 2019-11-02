24381bf8cc7efa016875bbd97f1c3d8c54e8bb51
胡佳露 <974520639@qq.com>
2019-11-02 23:09:52 +0800

    Create the class of SocketOpt at 2019-8-1

---
+++ SocketOpt.h
@@ -0,0 +1,21 @@
+//
+// Created by Jialu  Hu on 2019-8-2.
+//
+
+#ifndef UNTITLED_SOCKETOPT_H
+#define UNTITLED_SOCKETOPT_H
+
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <assert.h>
+#include <fcntl.h>
+namespace SocketOpt{
+    int socket();
+    void bind(int sockfd, struct sockaddr_in *addr);
+    void listen(int socketfd);
+    int accept(int sockfd);
+    int setnonblocking(int fd);
+    void socketpair(int wakeup[]);
+}
+#endif //UNTITLED_SOCKETOPT_H
